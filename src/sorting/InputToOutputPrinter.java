03fa1a0b48d85f12b7a0efe7564dfa4cc5268443
pChupakhin <pchupakhin@griddynmics.com>
2022-01-07 10:13:30 -0800

    -

--- src/sorting/InputToOutputPrinter.java
+++ src/sorting/InputToOutputPrinter.java
@@ -1,3 +1,5 @@
+package sorting;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
--- src/sorting/Main.java
+++ src/sorting/Main.java
@@ -1,3 +1,5 @@
+package sorting;
+
 import java.io.IOException;
 
 public class Main {
